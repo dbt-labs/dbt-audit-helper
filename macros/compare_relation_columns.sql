356f7fbe00d7c2c592ff8c558aeda6cd7e0fea58
Alieu <alieu.sanneh@avande.com>
2021-05-19 12:44:19 -0700

    dispatch more macros

--- macros/compare_relation_columns.sql
+++ macros/compare_relation_columns.sql
@@ -1,4 +1,9 @@
 {% macro compare_relation_columns(a_relation, b_relation) %}
+{{ adapter.dispatch('compare_relation_columns', packages=audit_helper._get_audit_helper_namespaces())(a_relation, b_relation) }}
+{% endmacro %}
+
+{% macro default__compare_relation_columns(a_relation, b_relation) %}
+
 with a_cols as (
     {{ audit_helper.get_columns_in_relation_sql(a_relation) }}
 ),
