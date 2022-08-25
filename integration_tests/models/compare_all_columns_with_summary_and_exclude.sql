771664c43f7c4afdf0e06022513d0ef38bdb17ec
Leo Brown <leo.e.brown@gmail.com>
2022-08-25 09:32:45 -0700

    create dedicated seeds for compare_all_columns; correct with_summary_and_exclude expected results

--- integration_tests/models/compare_all_columns_with_summary_and_exclude.sql
+++ integration_tests/models/compare_all_columns_with_summary_and_exclude.sql
@@ -1,6 +1,6 @@
-{% set a_relation=ref('data_compare_relations__a_relation')%}
+{% set a_relation=ref('data_compare_all_columns__a_relation')%}
 
-{% set b_relation=ref('data_compare_relations__b_relation') %}
+{% set b_relation=ref('data_compare_all_columns__b_relation') %}
 
 {{ audit_helper.compare_all_columns(
     a_relation=a_relation,
