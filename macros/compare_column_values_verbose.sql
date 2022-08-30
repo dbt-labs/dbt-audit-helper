59fdea424e956a9bb2cd8e139923152e26f04c7b
Leo Brown <leo.e.brown@gmail.com>
2022-08-30 10:05:40 -0700

    cast column_to_compare to text to satisfy redshift; drop analyses to get redshift to pass locally

--- macros/compare_column_values_verbose.sql
+++ macros/compare_column_values_verbose.sql
@@ -13,7 +13,7 @@ b_query as (
 )
     select
         coalesce(a_query.{{ adapter.quote(primary_key) }}, b_query.{{ adapter.quote(primary_key) }}) as {{ primary_key }},
-        '{{ column_to_compare }}' as column_name,
+       '{{ column_to_compare }}'::text as column_name,
         a_query.{{ adapter.quote(column_to_compare) }} = b_query.{{ adapter.quote(column_to_compare) }} as perfect_match,
         a_query.{{ adapter.quote(column_to_compare) }} is null as null_in_a,
         b_query.{{ adapter.quote(column_to_compare) }} is null as null_in_b,
