32c51955ad7b95c33f4433cdfd9d2a74215cb949
Leo Brown <leo.e.brown@gmail.com>
2022-08-30 14:28:56 -0700

    remove all adapter quotes from compare_column_values_verbose

--- macros/compare_column_values_verbose.sql
+++ macros/compare_column_values_verbose.sql
@@ -12,20 +12,20 @@ b_query as (
     {{ b_query }}
 )
     select
-        coalesce(a_query.{{ adapter.quote(primary_key) }}, b_query.{{ adapter.quote(primary_key) }}) as {{ primary_key }},
+        coalesce(a_query.{{ primary_key }}, b_query.{{ primary_key }}) as {{ primary_key }},
        '{{ column_to_compare }}'::text as column_name,
-        a_query.{{ column_to_compare }} = b_query.{{ adapter.quote(column_to_compare) }} as perfect_match,
-        a_query.{{ adapter.quote(column_to_compare) }} is null as null_in_a,
-        b_query.{{ adapter.quote(column_to_compare) }} is null as null_in_b,
-        a_query.{{ adapter.quote(primary_key) }} is null as missing_from_a,
-        b_query.{{ adapter.quote(primary_key) }} is null as missing_from_b,
-        a_query.{{ adapter.quote(column_to_compare) }} != b_query.{{ adapter.quote(column_to_compare) }} and
-            (a_query.{{ adapter.quote(column_to_compare) }} is not null or b_query.{{ adapter.quote(column_to_compare) }} is not null)
+        a_query.{{ column_to_compare }} = b_query.{{ column_to_compare }} as perfect_match,
+        a_query.{{ column_to_compare }} is null as null_in_a,
+        b_query.{{ column_to_compare }} is null as null_in_b,
+        a_query.{{ primary_key }} is null as missing_from_a,
+        b_query.{{ primary_key }} is null as missing_from_b,
+        a_query.{{ column_to_compare }} != b_query.{{ column_to_compare }} and
+            (a_query.{{ column_to_compare }} is not null or b_query.{{ column_to_compare }} is not null)
           as conflicting_values
            -- considered a conflict if the values do not match AND at least one of the values is not null.
 
     from a_query
 
-    full outer join b_query on (a_query.{{ adapter.quote(primary_key) }} = b_query.{{ adapter.quote(primary_key) }})
+    full outer join b_query on (a_query.{{ primary_key }} = b_query.{{ primary_key }})
 
 {% endmacro %} 
