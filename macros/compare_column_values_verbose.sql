fb600af62741f3411c20a2aa2ffb12522a37b4ae
Leo Brown <leo.e.brown@gmail.com>
2022-08-30 22:18:59 -0700

    update cast text to cast string for bigquery

--- macros/compare_column_values_verbose.sql
+++ macros/compare_column_values_verbose.sql
@@ -13,7 +13,7 @@ b_query as (
 )
     select
         coalesce(a_query.{{ primary_key }}, b_query.{{ primary_key }}) as primary_key,
-        '{{ column_to_compare }}'::text as column_name,
+        cast('{{ column_to_compare }}' as string) as column_name,
         a_query.{{ column_to_compare }} = b_query.{{ column_to_compare }} as perfect_match,
         a_query.{{ column_to_compare }} is null as null_in_a,
         b_query.{{ column_to_compare }} is null as null_in_b,
