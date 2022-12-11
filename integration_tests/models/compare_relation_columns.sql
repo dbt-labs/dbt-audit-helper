0e65672461e2c5a18f5d976e5016e5a75805547e
Joel Labes <joel.labes@dbtlabs.com>
2022-12-12 12:42:01 +1300

    Add integration test for comparing columns

---
+++ integration_tests/models/compare_relation_columns.sql
@@ -0,0 +1,16 @@
+
+with audit_helper_results as (
+    {{ audit_helper.compare_relation_columns(
+        a_relation=ref('data_compare_relation_columns_a'),
+        b_relation=ref('data_compare_relation_columns_b')
+    ) }}
+)
+
+select 
+    column_name,
+    a_ordinal_position,
+    b_ordinal_position,
+    --not checking the specific datatypes, as long as they match/don't match as expected then that's still checking the audit behaviour
+    has_ordinal_position_match,
+    has_data_type_match
+from audit_helper_results
