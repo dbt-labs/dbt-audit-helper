e72925a87f6306c858b2397962a6073ed01ae911
Joel Labes <joel.labes@dbtlabs.com>
2022-12-12 14:33:40 +1300

    cast ordinals as ints

--- integration_tests/models/compare_relation_columns.sql
+++ integration_tests/models/compare_relation_columns.sql
@@ -7,9 +7,10 @@ with audit_helper_results as (
 )
 
 select 
-    cast(column_name as {{ dbt.type_string() }}) as column_name, --otherwise it is technically a "sql_identifier" type on Redshift
-    a_ordinal_position,
-    b_ordinal_position,
+    --These need to be cast, otherwise they are technically typed as "sql_identifier" or "cardinal_number" on Redshift
+    cast(column_name as {{ dbt.type_string() }}) as column_name, 
+    cast(a_ordinal_position as {{ dbt.type_int() }}) as a_ordinal_position,
+    cast(b_ordinal_position as {{ dbt.type_int() }}) as b_ordinal_position,
     --not checking the specific datatypes, as long as they match/don't match as expected then that's still checking the audit behaviour
     has_ordinal_position_match,
     has_data_type_match
