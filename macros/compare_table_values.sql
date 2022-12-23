ac9650440c4278113336a38ab45476e998bedc68
Dave Lyons <davesgonechina@gmail.com>
2022-12-23 11:41:10 -0500

    break out cloud_safe_print_table, use do

--- macros/compare_table_values.sql
+++ macros/compare_table_values.sql
@@ -41,29 +41,8 @@ dbt run-operation compare_table_values --args '{"old_table": "my_production_data
         ) %}
 
         {% set audit_results = run_query(audit_query) %}
-
-        {% cloud_safe_print_table(audit_results) %}
+        {% do cloud_safe_print_table(audit_results) %}
     {% endfor %}
 {% endif %}
 
-{% endmacro %}
-
-{% macro is_cloud() %}
-    {# In the dbt Cloud context, `DBT_ENV` will always resolve to "prod" #}
-    {% set is_cloud = env_var("DBT_ENV", "core") == "prod" %}
-    {{ return(is_cloud) }}
-{% endmacro %}
-
-{% macro cloud_safe_print_table(audit_results) %}
-{% if execute %}
-    {% if is_cloud() %}
-        {% do log(audit_results.column_names, info=True) %}
-        {% for row in audit_results.rows %}
-            {% do log(row.values(), info=True) %}
-        {% endfor %}
-    {% else %}
-        {% do audit_results.print_table() %}
-        {{ log("", info=True) }}
-    {% endif %}
-{% endif %}
 {% endmacro %}
