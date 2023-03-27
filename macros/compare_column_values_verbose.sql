464c211391d53a609dc06ff5730de9b51505c5de
Leo Folsom <leoebfolsom@gmail.com>
2023-03-27 14:04:51 -0700

    account for comparison of null value to missing value

--- macros/compare_column_values_verbose.sql
+++ macros/compare_column_values_verbose.sql
@@ -20,9 +20,12 @@ b_query as (
             '{{ column_to_compare }}' as column_name,
         {% endif %}
 
-        coalesce(a_query.{{ column_to_compare }} = b_query.{{ column_to_compare }},
-          (a_query.{{ column_to_compare }} is null and b_query.{{ column_to_compare }} is null),
-          false) as perfect_match,
+        coalesce(
+            a_query.{{ column_to_compare }} = b_query.{{ column_to_compare }} and 
+                a_query.{{ primary_key }} is not null and b_query.{{ primary_key }} is not null,
+            (a_query.{{ column_to_compare }} is null and b_query.{{ column_to_compare }} is null),
+            false
+        ) as perfect_match,
         a_query.{{ column_to_compare }} is null and a_query.{{ primary_key }} is not null as null_in_a,
         b_query.{{ column_to_compare }} is null and b_query.{{ primary_key }} is not null as null_in_b,
         a_query.{{ primary_key }} is null as missing_from_a,
