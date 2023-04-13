7bee79a4df716c24bc23b76d76210974ef2d5ba8
Grace Goheen <grace.goheen@dbtlabs.com>
2023-04-13 16:22:18 -0700

    Add support for concatenated sql as pk and integration tests

---
+++ integration_tests/models/compare_all_columns_concatenated_sql_as_primary_key_without_summary.sql
@@ -0,0 +1,10 @@
+{% set a_relation=ref('data_compare_all_columns__market_of_choice_produce__concatenated_pk')%}
+
+{% set b_relation=ref('data_compare_all_columns__albertsons_produce__concatenated_pk') %}
+
+{{ audit_helper.compare_all_columns(
+    a_relation=a_relation,
+    b_relation=b_relation,
+    primary_key="produce_category||id",
+    summarize=false
+) }}
