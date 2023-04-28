3aa5f6d81aade8395106e7e729fdafbba85213c2
Grace Goheen <grace.goheen@dbtlabs.com>
2023-04-27 17:03:28 -0700

    fixed integration test for alias

--- integration_tests/models/report_compare_relations_without_identifiers.sql
+++ integration_tests/models/report_compare_relations_without_identifiers.sql
@@ -1,8 +1,7 @@
 -- depends_on: {{ ref('model_a') }}
--- depends_on: {{ ref('model_b') }}
 
 {{ audit_helper.generate_audit_report(
-    model_list = ['model_a', 'model_b'],
+    model_list = ['model_a'],
     compare_database = this.database, 
     compare_schema = this.schema
 ) }}
