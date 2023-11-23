36d0885805f5575e7bbcc318cfbca036545fa972
Eva Bilski <eva.bilski@mobsuccess.com>
2023-11-23 17:55:34 +0100

    remove group by static value

--- macros/compare_column_values.sql
+++ macros/compare_column_values.sql
@@ -50,7 +50,7 @@ aggregated as (
         count(*) as count_records
     from joined
 
-    group by '{{ column_to_compare }}', match_status, match_order
+    group by match_status, match_order
 )
 
 select
