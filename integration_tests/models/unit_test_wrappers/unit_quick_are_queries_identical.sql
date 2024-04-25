0d766d67ccf36f8de99882d09d52336378aed49e
Joel Labes <joel.labes@dbtlabs.com>
2024-04-25 18:42:26 +1200

    add quick_are_queries_identical and unit tests

---
+++ integration_tests/models/unit_test_wrappers/unit_quick_are_queries_identical.sql
@@ -0,0 +1,8 @@
+{{ 
+    audit_helper.quick_are_queries_identical(
+        "select * from " ~ ref('unit_test_model_a'),
+        "select * from " ~ ref('unit_test_model_b'),
+        columns=var('quick_are_queries_identical_cols'),
+        event_time=var('quick_are_queries_identical_event_time')
+    ) 
+}}
---
+++ macros/quick_are_queries_identical.sql
@@ -0,0 +1,42 @@
+{% macro quick_are_queries_identical(query_a, query_b, columns=[], event_time=None) %}
+    {{ return (adapter.dispatch('quick_are_queries_identical', 'audit_helper')(query_a, query_b, columns, event_time)) }}
+{% endmacro %}
+
+{% macro default__quick_are_queries_identical(query_a, query_b, columns, event_time) %}
+    {% set joined_cols = columns | join(", ") %}
+    {% if event_time %}
+        {% set event_time_props = audit_helper.get_comparison_bounds(a_query, b_query, event_time) %}
+    {% endif %}
+
+    select count(hash_result) = 1 as are_tables_identical
+    from (
+        select hash_agg(joined_cols) as hash_result
+        from ({{ query_a }})
+        {% if event_time_props %}
+            where {{ event_time_props["event_time"] }} >= '{{ event_time_props["min_event_time"] }}'
+            and {{ event_time_props["event_time"] }} <= '{{ event_time_props["max_event_time"] }}'
+        {% endif %}
+
+        union 
+        
+        select hash_agg(joined_cols) as hash_result
+        from analytics_dev.dbt_jlabes.fct_dbt_invocations
+        {% if event_time_props %}
+            where {{ event_time_props["event_time"] }} >= '{{ event_time_props["min_event_time"] }}'
+            and {{ event_time_props["event_time"] }} <= '{{ event_time_props["max_event_time"] }}'
+        {% endif %}
+
+    ) as hashes
+{% endmacro %}
+
+{% macro is_quick_are_queries_identical_supported() %}
+    {{ return (adapter.dispatch('is_quick_are_queries_identical_supported', 'audit_helper')()) }}
+{% endmacro %}
+
+{% macro default__is_quick_are_queries_identical_supported() %}
+    {{ return (False) }}
+{% endmacro %}
+
+{% macro snowflake__is_quick_are_queries_identical_supported() %}
+    {{ return (True) }}
+{% endmacro %}
