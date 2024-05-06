58751e6495118193fbb1386e8b6a97992aa41775
Joel Labes <joel.labes@dbtlabs.com>
2024-05-06 16:34:07 +1200

    Add test for multiple PKs

--- integration_tests/models/unit_test_wrappers/unit_reworked_compare.sql
+++ integration_tests/models/unit_test_wrappers/unit_reworked_compare.sql
@@ -2,7 +2,7 @@
     audit_helper.reworked_compare(
         "select * from " ~ ref('unit_test_model_a'),
         "select * from " ~ ref('unit_test_model_b'),
-        primary_key_columns=['id'],
+        primary_key_columns=var('reworked_compare__primary_key_columns'),
         columns=var('reworked_compare__columns'),
         event_time=var('reworked_compare__event_time')
     ) 
