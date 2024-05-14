311fbdcd6ef256344540ba35bf61ccc7dd386083
Joel Labes <joel.labes@dbtlabs.com>
2024-05-14 16:48:30 +1200

    cross-db support

--- integration_tests/models/unit_test_placeholder_models/unit_test_model_b_more_cols.sql
+++ integration_tests/models/unit_test_placeholder_models/unit_test_model_b_more_cols.sql
@@ -1,1 +1,1 @@
-select 12 as id, 22 as id_2, 'xyz' as col1, 'tuv' as col2, 123 as col3, getdate() as created_at, 'pineapple' as pizza
+select 12 as id, 22 as id_2, 'xyz' as col1, 'tuv' as col2, 123 as col3, {{ dbt.current_timestamp() }} as created_at, 'pineapple' as pizza
