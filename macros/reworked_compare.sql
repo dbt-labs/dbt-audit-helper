df95fcae469a998b8098d3429daa3a6ed99d4e28
Joel Labes <joel.labes@dbtlabs.com>
2024-05-17 11:50:52 +1200

    add row status of nonunique_pk

--- macros/reworked_compare.sql
+++ macros/reworked_compare.sql
@@ -48,6 +48,7 @@
         select 
             *,
             case 
+                when max(dbt_audit_pk_row_num) over (partition by dbt_audit_surrogate_key) > 1 then 'nonunique_pk'
                 when in_a and in_b then 'identical'
                 when {{ dbt.bool_or('in_a') }} over (partition by dbt_audit_surrogate_key, dbt_audit_pk_row_num) 
                     and {{ dbt.bool_or('in_b') }} over (partition by dbt_audit_surrogate_key, dbt_audit_pk_row_num)
