9523db8675d112896abad65f613cab51962e45c3
Joel Labes <joel.labes@dbtlabs.com>
2024-05-17 12:55:22 +1200

    remove redundant test and wrapper model

--- integration_tests/models/unit_test_wrappers/unit_reworked_compare_column_details_mismatch.sql
+++
@@ -1,9 +0,0 @@
-{{ 
-    audit_helper.reworked_compare(
-        "select * from " ~ ref('unit_test_model_a'),
-        "select * from " ~ ref('unit_test_model_b_more_cols'),
-        primary_key_columns=['id'],
-        columns=var('reworked_compare__columns'),
-        event_time=var('reworked_compare__event_time')
-    ) 
-}}
