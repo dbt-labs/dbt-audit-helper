1a8c2e1ff3c8b99ad45d7302159ddf70205afaba
Joel Labes <joel.labes@dbtlabs.com>
2024-05-28 11:38:29 +1200

    add BQ support for qucik are queries identical

--- integration_tests/models/unit_test_wrappers/unit_quick_are_queries_identical.sql
+++ integration_tests/models/unit_test_wrappers/unit_quick_are_queries_identical.sql
@@ -1,4 +1,4 @@
-{{ config(tags=['skip' if (target.type in ['redshift', 'bigquery', 'postgres', 'databricks']) else 'runnable']) }}
+{{ config(tags=['skip' if (target.type in ['redshift', 'postgres', 'databricks']) else 'runnable']) }}
 
 {{ 
     audit_helper.quick_are_queries_identical(
