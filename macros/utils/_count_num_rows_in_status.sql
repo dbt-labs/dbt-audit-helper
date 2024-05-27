88f2be84545576cd6a1360f1f76d5fc79da31290
Joel Labes <joel.labes@dbtlabs.com>
2024-05-27 14:52:49 +1200

    It's a postgres problem, not a redshift problem

--- macros/utils/_count_num_rows_in_status.sql
+++ macros/utils/_count_num_rows_in_status.sql
@@ -7,19 +7,11 @@
 {% endmacro %}
 
 {%- macro bigquery___count_num_rows_in_status() -%}
-    {{ audit_helper._count_num_rows_in_status_concat() }}
+    count(distinct {{ dbt.concat(["dbt_audit_surrogate_key", "dbt_audit_pk_row_num"]) }}) over (partition by dbt_audit_row_status)
 {% endmacro %}
 
 {%- macro postgres___count_num_rows_in_status() -%}
-    {{ audit_helper._count_num_rows_in_status_concat() }}
-{% endmacro %}
-
-{%- macro redshift___count_num_rows_in_status() -%}
-    {#- Redshift doesn't support count(distinct) inside of window functions :( -#}
+    {#- Postgres/Redshift doesn't support count(distinct) inside of window functions :( -#}
     {#- modified rows are the only ones that return two rows per PK/row num pairing, so just need to be halved -#}
     (count(*) over (partition by dbt_audit_row_status)) / case when dbt_audit_row_status = 'modified' then 2 else 1 end
-{% endmacro %}
-
-{% macro _count_num_rows_in_status_concat() %}
-    count(distinct {{ dbt.concat(["dbt_audit_surrogate_key", "dbt_audit_pk_row_num"]) }}) over (partition by dbt_audit_row_status)
 {% endmacro %}
