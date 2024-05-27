317e4d706530879390d02a2d2b911e0f10e7363b
Joel Labes <joel.labes@dbtlabs.com>
2024-05-27 15:44:38 +1200

    Found a better workaround for missing count distinct window

--- macros/utils/_count_num_rows_in_status.sql
+++ macros/utils/_count_num_rows_in_status.sql
@@ -11,7 +11,18 @@
 {% endmacro %}
 
 {%- macro postgres___count_num_rows_in_status() -%}
-    {#- Postgres/Redshift doesn't support count(distinct) inside of window functions :( -#}
-    {#- modified rows are the only ones that return two rows per PK/row num pairing, so just need to be halved -#}
-    (count(*) over (partition by dbt_audit_row_status)) / case when dbt_audit_row_status = 'modified' then 2 else 1 end
+    _count_num_rows_in_status_without_distinct_window_func()
+{% endmacro %}
+
+{%- macro databricks___count_num_rows_in_status() -%}
+    _count_num_rows_in_status_without_distinct_window_func()
+{% endmacro %}
+
+{% macro _count_num_rows_in_status_without_distinct_window_func() %}
+    {#- Some platforms don't support count(distinct) inside of window functions -#}
+    {#- You can get the same outcome by dense_rank, assuming no nulls (we've already handled that) #}
+    {# https://stackoverflow.com/a/22347502 -#}
+    dense_rank() over (partition by dbt_audit_row_status order by dbt_audit_surrogate_key, dbt_audit_pk_row_num)
+    + dense_rank() over (partition by dbt_audit_row_status order by dbt_audit_surrogate_key desc, dbt_audit_pk_row_num desc)
+    - 1
 {% endmacro %}
