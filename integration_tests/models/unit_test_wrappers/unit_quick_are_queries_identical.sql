fc1c5cd47d20f11ec7a9e0821f83677021035e79
Joel Labes <joel.labes@dbtlabs.com>
2024-05-28 15:58:13 +1200

    add where clause to check we're not assuming its absence

--- integration_tests/models/unit_test_wrappers/unit_quick_are_queries_identical.sql
+++ integration_tests/models/unit_test_wrappers/unit_quick_are_queries_identical.sql
@@ -2,8 +2,8 @@
 
 {{ 
     audit_helper.quick_are_queries_identical(
-        "select * from " ~ ref('unit_test_model_a'),
-        "select * from " ~ ref('unit_test_model_b'),
+        "select * from " ~ ref('unit_test_model_a') ~ " where 1=1",
+        "select * from " ~ ref('unit_test_model_b') ~ " where 1=1",
         columns=var('quick_are_queries_identical_cols'),
         event_time=var('quick_are_queries_identical_event_time')
     ) 
--- integration_tests/models/unit_test_wrappers/unit_reworked_compare.sql
+++ integration_tests/models/unit_test_wrappers/unit_reworked_compare.sql
@@ -1,7 +1,7 @@
 {{ 
     audit_helper.reworked_compare(
-        "select * from " ~ ref('unit_test_model_a'),
-        "select * from " ~ ref('unit_test_model_b'),
+        "select * from " ~ ref('unit_test_model_a') ~ " where 1=1",
+        "select * from " ~ ref('unit_test_model_b') ~ " where 1=1",
         primary_key_columns=var('reworked_compare__primary_key_columns'),
         columns=var('reworked_compare__columns'),
         event_time=var('reworked_compare__event_time')
