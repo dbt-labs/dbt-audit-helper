9772be72fe2ef6108bf59855a1b7b61715fb4a55
Joel Labes <joel.labes@dbtlabs.com>
2024-05-28 17:15:27 +1200

    json_build_object doesn't work on rs

--- integration_tests/macros/unit_tests/struct_generation_macros.sql
+++ integration_tests/macros/unit_tests/struct_generation_macros.sql
@@ -5,8 +5,6 @@
         json_object
     {%- elif target.type == 'databricks' -%}
         map
-    {%- elif target.type == 'redshift' -%}
-        json_build_object
     {%- elif execute -%}
         {# Only raise exception if it's actually being called, not during parsing #}
         {%- do exceptions.raise_compiler_error("Unknown adapter '"~ target.type ~ "'")-%}
