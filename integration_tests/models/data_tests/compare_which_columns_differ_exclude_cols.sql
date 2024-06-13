c5402cb7c28dc332e6b5bf5481207a6f2a7e0141
Joel Labes <joel.labes@dbtlabs.com>
2024-06-13 15:14:10 +1200

    uppercase in the right place

--- integration_tests/models/data_tests/compare_which_columns_differ_exclude_cols.sql
+++ integration_tests/models/data_tests/compare_which_columns_differ_exclude_cols.sql
@@ -2,6 +2,13 @@
 
 {% set b_relation=ref('data_compare_which_columns_differ_b') %}
 
+{% set pk_cols = ['id'] %}
+{% set cols = ['id','value_changes','becomes_not_null','does_not_change'] %}
+
+{% if target.type == 'snowflake' %}
+    {% set pk_cols = pk_cols | map("upper") | list %}
+    {% set cols = cols | map("upper") | list %}
+{% endif %}
 
 select 
     lower(column_name) as column_name,
@@ -11,8 +18,8 @@ from (
     {{ audit_helper.compare_which_relation_columns_differ(
         a_relation=a_relation,
         b_relation=b_relation,
-        primary_key_columns=['id'],
-        columns=['id','value_changes','becomes_not_null','does_not_change']
+        primary_key_columns=pk_cols,
+        columns=cols
     ) }}
 
 ) as macro_output
