0904686ee41a4aca6705cb5805709da533682537
Joel Labes <joel.labes@dbtlabs.com>
2024-06-13 15:41:50 +1200

    ughhhhh (custom tests for SNOW)

--- integration_tests/models/unit_test_wrappers/unit_compare_which_query_columns_differ.sql
+++ integration_tests/models/unit_test_wrappers/unit_compare_which_query_columns_differ.sql
@@ -1,17 +1,9 @@
-{% set pk_cols = var('primary_key_columns_var') %}
-{% set cols = var('columns_var') %}
-
-{% if target.type == 'snowflake' %}
-    {% set pk_cols = pk_cols | map("upper") | list %}
-    {% set cols = cols | map("upper") | list %}
-{% endif %}
-
 {{ 
     audit_helper.compare_which_query_columns_differ(
         a_query = "select * from " ~ ref('unit_test_model_a') ~ " where 1=1",
         b_query = "select * from " ~ ref('unit_test_model_b') ~ " where 1=1",
-        primary_key_columns = pk_cols, 
-        columns = cols,
+        primary_key_columns = var('primary_key_columns_var'), 
+        columns = var('columns_var'),
         event_time = var('event_time_var')
     )
 }}
